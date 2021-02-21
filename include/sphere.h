53cb4059797d5fc50eb1bba453c72f6d3fc3f5d8
longwatermelon <qianhansenb@gmail.com>
2021-02-20 21:12:32 -0700

    spheres

---
+++ include/sphere.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+class Sphere
+{
+public:
+	float x, y, z;
+	int r;
+
+	Sphere(float x, float y, float z, int r)
+		: x(x), y(y), z(z), r(r) {}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "../include/object.h"
+#include "../include/sphere.h"
+
 #include <graphics.h>
 
 std::vector<Object> objects;
+std::vector<Sphere> spheres;
 
 
 bool within_x(float x, Object& obj)
@@ -11,7 +14,7 @@ bool within_x(float x, Object& obj)
 
 bool within_y(float y, Object& obj)
 {
-	return (y > obj.y && y < obj.y + obj.yl);
+	return (y >= obj.y && y <= obj.y + obj.yl);
 }
 
 bool within_z(float z, Object& obj)
@@ -19,10 +22,18 @@ bool within_z(float z, Object& obj)
 	return (z >= obj.z && z <= obj.z + obj.zl);
 }
 
+bool inside_sphere(float x, float y, float z, Sphere& s)
+{
+	float xf = (x - s.x) * (x - s.x);
+	float yf = (y - s.y) * (y - s.y);
+	float zf = (z - s.z) * (z - s.z);
+
+	return sqrt(xf + yf + zf) <= s.r;
+}
+
 
 int main()
 {
-
 	int screen_w = 120;
 	int screen_h = 30;
 
@@ -33,11 +44,15 @@ int main()
 	float pva = 0.0f;
 	float fov = 1.0f;
 
-	float render_dist = 25.0f;
+	float render_dist = 35.0F;
 
-	objects.push_back(Object(0.0f, 0.0f, 4.0f));
+	/*objects.push_back(Object(0.0f, 0.0f, 4.0f));
 	objects.push_back(Object(0.0f, 0.0f, 3.0f));
-	objects.push_back(Object(1.0f, 0.0f, 4.0f));
+	objects.push_back(Object(1.0f, 0.0f, 4.0f));*/
+
+	spheres.push_back(Sphere(0.0f, 0.0f, 4.0f, 2));
+	spheres.push_back(Sphere(5.0f, 2.0f, 3.0f, 3));
+	spheres.push_back(Sphere(0.0f, -1.0f, 0.0f, 1));
 	
 
 	while (true)
@@ -117,14 +132,23 @@ int main()
 							break;
 						}
 					}
+
+					for (Sphere& s : spheres)
+					{
+						if (inside_sphere(tx, ty, tz, s))
+						{
+							hit_obj = true;
+							break;
+						}
+					}
 				}
 				
 
 				if (hit_obj)
 				{
 					char shade = ' ';
-					if (dist < render_dist / 10)      shade = '#';
-					else if (dist < render_dist / 7) shade = '=';
+					if (dist < render_dist / 11)      shade = '#';
+					else if (dist < render_dist / 8) shade = '=';
 					else if (dist < render_dist / 6) shade = ':';
 					else if (dist < render_dist / 5) shade = '.';
 
